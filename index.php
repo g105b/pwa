0f0855e73bcd45788f7181669443aaab1d16afd3
Anna-Maria Haslinger <anna-maria.haslinger@gmx.de>
2022-03-08 12:05:28 +0100

    Titel, Nav & Input Elemente

--- index.php
+++ index.php
@@ -3,20 +3,32 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Service Worker Example</title>
+	<title>Klausurenübersicht</title>
 	<link rel="manifest" href="/manifest.json" />
 	<link href="/style.css" rel="stylesheet" />
 </head>
 
 <body>
-<h1>Service Worker Example</h1>
-<a href="page1.php">Page 1</a>
-<br/><br/>
-<a href="page2.php">Page 2</a>
-<br/><br/>
+<h1>Klausurenübersicht</h1>
+	<nav class="menu">
+		<li><a href="page1.php">Listenansicht</a></li>
+		<li><a href="page2.php">Kalenderansicht</a></li>
+	</nav>
 
 <div>
-	<p>Before you enable the PWA, check out how slow this website is.</p>
+	<p>Füge eine neue Klausur hinzu:</p>
+	<input></input>
+	<input></input>
+	<input></input>
+	<input></input>
+	<button>Hinzufügen</button>
+</div>
+
+<div>
+	<p>hier werden die Klausuren angezeigt</p>
+</div>
+
+<div>
 	<button id="enable">Enable the PWA</button>
 </div>
 
